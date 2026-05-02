097eeb456cdd0937d3591d09a77ee123cbcc9104
Reikyuu09 <muflihalrasyid@gmail.com>
2026-05-02 20:10:02 +0700

    udh

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/login", userController.login);
+router.post("/signIn", userController.signIn);
 
 module.exports = router;
